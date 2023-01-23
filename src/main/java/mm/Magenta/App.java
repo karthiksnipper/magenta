7a01b43e70330675b39a005f30379d203918ad35
karthiksnipper <karthik.lifeissweet@gmail.com>
2023-01-23 10:13:39 +0530

    new load

---
+++ src/main/java/mm/Magenta/App.java
@@ -0,0 +1,13 @@
+package mm.Magenta;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/mm/Magenta/Super.java
@@ -0,0 +1,32 @@
+package mm.Magenta;
+
+public class Super {
+
+	private void Meth() { System.out.println("m1");
+
+	}
+	
+	private void cryst() {
+		System.out.println("m2");
+	}
+	
+	private void blueStone() {
+		System.out.println("m3");
+	}
+	
+	
+	private void india() { System.out.println("india");
+
+	}
+	
+	private void america() {
+		System.out.println("USA");
+	}
+	
+	private void france() {
+		System.out.println("France");
+	}
+	
+
+	
+}
